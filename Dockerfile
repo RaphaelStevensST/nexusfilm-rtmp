5bc73f034c582bdbbc29371e6cee1b8733e56255
RaphaelStevensST <157969217+RaphaelStevensST@users.noreply.github.com>
2026-03-08 11:44:35 -0300

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM bluenviron/mediamtx:latest-ffmpeg
 COPY mediamtx.yml /mediamtx.yml
+COPY start.sh /start.sh
+RUN chmod +x /start.sh
 EXPOSE 8888
 EXPOSE 1935
+CMD ["/bin/sh", "/start.sh"]
--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-exec /mediamtx /mediamtx.yml
+HLS_PORT=${PORT:-8888}
+sed -i "s/:8888/:${HLS_PORT}/" /mediamtx.yml
+echo "Iniciando MediaMTX na porta HLS $HLS_PORT e RTMP 1935"
+exec /mediamtx
